09829afd71cf35ecef7c7c890b6bd865f95edfcc
lovetosurf <melinda@melindamoree.com>
2018-04-02 16:06:37 -0700

    Coursera Week 3 Problem
    Melinda's solution to Week 3 programming problem 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## this function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## tell when cacheSolve as run
+  inv <-NULL
+  ##store the matrix
+  set <- function(y) {
+    ##put matrix 'x' into cache
+    x <<- y
+    ##
+    inv <<- NULL
+  }
+  ##return the matrix
+  get <- function() x
+  setinv <- function(inverse) inv<<-inverse
+  getinv <- function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## this function computes the inverse of the special "matrix" returned by 
+##makeCacheMatrix above. If the inverse has already been calulated (and the 
+##matrix has not changed), then the cachesolve should retrieve the inverse
+##from the cache
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return matrix that is inverse of x
+  inv <- x$getinv()
+  if(!is.null(inv)){
+    message("retrieving cached inverse matrix")
+    return(inv)
+  }
+  data_matrx <- x$get()
+  inv <-solve(data_matrx)
+  x$setinv(inv)
+  inv
 }
